c0489ecda3ab01029b8bfc7a059fd0f7c89770e3
kervindholah <kervin92310@hotmail.fr>
2024-07-13 19:20:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NasaService } from './nasa.service'; // Import du service
+import { CommonModule } from '@angular/common'; 
+import { provideHttpClient } from '@angular/common/http';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nasa-image-app';
+  imgOfTheDay: string | undefined; // Déclaration de la propriété
+
+  constructor(private nasaService: NasaService) {} // Injection du service
+
+  ngOnInit() {
+    this.nasaService.getImageOfTheDay().subscribe(data => {
+      this.imgOfTheDay = data.url;
+    });
+  }
 }
